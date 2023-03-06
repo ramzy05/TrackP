ab5153d659d6aa159599e66ce0efe773033a3817
R-Win <ramsesaguegang@gmail.com>
2023-03-06 01:22:41 +0100

    layout de la section administrateur ok !

--- client/src/components/MenuItem/index.jsx
+++ client/src/components/MenuItem/index.jsx
@@ -13,7 +13,7 @@ const MenuItem = ({type, link, text,isActive, setIsActive, isCollapsed, setIsCol
 		<a href={link} onClick={e=> handleOnClick()}
       title={isCollapsed && text[0].toUpperCase()+text.slice(1)}
       className={`menu-link flex items-center ${!isCollapsed && 'pl-4 py-4 gap-2 font-medium text-base rounded-2xl'}
-      ${active && !isCollapsed?'text-white bg-accent':'text-primary'}
+      ${active && !isCollapsed?'text-white bg-accent':` ${active?'text-accent':'text-primary'}`}
       ${isCollapsed && 'justify-center'}
     `}
 
